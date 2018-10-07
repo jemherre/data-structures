9bdb6479cf44158738e8d6e710ae0dc20306fd6d
Jessica Herrera <jherrera@fb.com>
2018-10-07 00:44:54 -0700

    implemented stack, but will go back to solve constructor error

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  //properties
+  someInstance.storage = {};
+  someInstance.objSize = 0;
+
+  _.extend(someInstance,Stack.stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+Stack.stackMethods = {};
+
+Stack.stackMethods.push = function(value){
+  this.storage[this.objSize] = value;
+  this.objSize++;
+};
+
+Stack.stackMethods.pop = function(){
+  if(this.objSize === 0) return;
+  var value = this.storage[this.objSize -1];
+  this.storage[this.objSize -1] == undefined;//clear elem
+  this.objSize--;
+  return value;
+};
+
+Stack.stackMethods.size = function(){
+  return this.objSize;
+};
 
 
