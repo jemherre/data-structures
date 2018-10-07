a2e1f862a54115cc89e8991577c1c43ac8536b68
Jessica Herrera <jherrera@fb.com>
2018-10-06 23:54:01 -0700

    implemented queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
+    console.log("IN >> ",JSON.stringify(storage),size);
   };
 
   someInstance.dequeue = function() {
+    if(size === 0) return;
+    var value = storage['0'];//save first elem
+    for(var i = 0; i < size; i++){
+      storage[i] = storage[i+1];
+    }
+    storage[size] = undefined;
+    size--;
+    return value;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
